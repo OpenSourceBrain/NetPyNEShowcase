da0b1c87ba581f31819924080db50a80740126e5
Padraig Gleeson <p.gleeson@gmail.com>
2016-05-16 17:38:40 +0100

    Adding generated jnml netpyne code
    Adding initial hh example

---
+++ regenerateAndTest.sh
@@ -0,0 +1,29 @@
+##  Regenerate NetPyNE from NeuroML 2
+
+cd NeuroML2
+
+jnml LEMS_2007One.xml -netpyne
+
+cp *.mod *_netpyne.py *definition.py ../NetPyNE
+
+
+##  Test Izh
+
+cd ../NetPyNE
+
+nrnivmodl
+python LEMS_2007One_netpyne.py
+
+
+##  Test NetPyNE example
+
+cd HHSmall
+
+python HH_run.py -nogui
+
+
+##  Done
+
+cd ../..
+
+
