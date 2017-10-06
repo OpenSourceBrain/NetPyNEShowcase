560f794d0b348fec2aa6e9d80832be5f5a8bec60
Padraig Gleeson <p.gleeson@gmail.com>
2017-10-06 11:23:31 +0100

    Regenerated

--- NetPyNE/HHSmall/HHSmall.py
+++ NetPyNE/HHSmall/HHSmall.py
@@ -54,7 +54,7 @@ netParams.connParams['PYR->PYR'] = {
     'weight': 0.002,                    # weight of each connection
     'delay': '0.2+normal(13.0,1.4)',     # delay min=0.2, mean=13.0, var = 1.4
     'threshold': 10,                    # threshold
-    'convergence': 'uniform(1,15)'}    # convergence (num presyn targeting postsyn) is uniformly distributed between 1 and 15
+    'convergence': pop_size-1}    # convergence (num presyn targeting postsyn) is uniformly distributed between 1 and 15
 
 
 ###############################################################################
