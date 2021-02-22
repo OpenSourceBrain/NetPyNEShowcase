d5c9ca0ea801b04323d37a9f106f2a3bfe1ba485
Padraig Gleeson <p.gleeson@gmail.com>
2021-02-22 17:30:41 +0000

    Regenerated with latest nml py libs

--- NetPyNE/LEMS_2007One_netpyne.py
+++ NetPyNE/LEMS_2007One_netpyne.py
@@ -28,110 +28,130 @@ import sys
 import time
 import datetime
 
-setup_start = time.time()
+class NetPyNESimulation():
+
+    def __init__(self, tstop, dt, seed=123456789, save_json=False):
+
+        self.setup_start = time.time()
+        
+
+        ###############################################################################
+        # NETWORK PARAMETERS
+        ###############################################################################
+
+        self.nml2_file_name = 'NET_2007One.net.nml'
+
+        ###############################################################################
+        # SIMULATION PARAMETERS
+        ###############################################################################
+
+        self.simConfig = specs.SimConfig()   # object of class SimConfig to store the simulation configuration
+
+        # Simulation parameters
+        self.simConfig.duration = self.simConfig.tstop = tstop # Duration of the simulation, in ms
+        self.simConfig.dt = dt # Internal integration timestep to use
+
+        # Seeds for randomizers (connectivity, input stimulation and cell locations)
+        # Note: locations and connections should be fully specified by the structure of the NeuroML,
+        # so seeds for conn & loc shouldn't affect networks structure/behaviour
+        self.simConfig.seeds = {'conn': 0, 'stim': 123456789, 'loc': 0} 
+
+        self.simConfig.createNEURONObj = 1  # create HOC objects when instantiating network
+        self.simConfig.createPyStruct = 1  # create Python structure (simulator-independent) when instantiating network
+        self.simConfig.verbose = False  # show detailed messages 
+        
+        # Recording 
+        self.simConfig.recordCells = ['all']  
+        self.simConfig.recordTraces = {}
+        self.simConfig.saveCellSecs=False
+        self.simConfig.saveCellConns=False
+        self.simConfig.gatherOnlySimData=True 
+
+                # For saving to file: exIzh.dat (ref: of0)
+                                
+        # Column: v: Pop: RS_pop; cell: 0; segment id: $oc.segment_id; segment name: soma; Neuron loc: soma(0.5); value: v (v)
+        self.simConfig.recordTraces['of0_RS_pop_0_soma_v'] = {'sec':'soma','loc':0.5,'var':'v','conds':{'pop':'RS_pop','cellLabel':0}}
+                        
+        
+        self.simConfig.plotCells = ['all']
+
+        self.simConfig.recordStim = True  # record spikes of cell stims
+        self.simConfig.recordStep = self.simConfig.dt # Step size in ms to save data (eg. V traces, LFP, etc)
+
+        # Analysis and plotting, see http://neurosimlab.org/netpyne/reference.html#analysis-related-functions
+        self.simConfig.analysis['plotRaster'] = False  # Plot raster
+        self.simConfig.analysis['plot2Dnet'] = False  # Plot 2D net cells and connections
+        self.simConfig.analysis['plotSpikeHist'] = False # plot spike histogram
+        self.simConfig.analysis['plotConn'] = False # plot network connectivity
+        self.simConfig.analysis['plotSpikePSD'] = False # plot 3d architecture
+
+        # Saving
+        self.simConfig.filename = 'net1.txt'  # Set file output name
+        self.simConfig.saveFileStep = self.simConfig.dt # step size in ms to save data to disk
+        # self.simConfig.saveDat = True # save to dat file
+        self.simConfig.saveJson = save_json # save to json file
 
 
-###############################################################################
-# NETWORK PARAMETERS
-###############################################################################
+    def run(self):
 
-nml2_file_name = 'NET_2007One.net.nml'
+        ###############################################################################
+        # IMPORT & RUN
+        ###############################################################################
 
-###############################################################################
-# SIMULATION PARAMETERS
-###############################################################################
+        print("Running a NetPyNE based simulation for %sms (dt: %sms) at %s degC"%(self.simConfig.duration, self.simConfig.dt, self.simConfig.hParams['celsius']))
 
-simConfig = specs.SimConfig()   # object of class SimConfig to store the simulation configuration
+        self.setup_sim_start = time.time()
+        self.gids = sim.importNeuroML2SimulateAnalyze(self.nml2_file_name,self.simConfig)
 
-# Simulation parameters
-simConfig.duration = simConfig.tstop = 520.0 # Duration of the simulation, in ms
-simConfig.dt = 0.001 # Internal integration timestep to use
+        self.sim_end = time.time()
+        self.setup_sim_time = self.sim_end - self.setup_sim_start
+        print("Finished NetPyNE simulation in %f seconds (%f mins)..."%(self.setup_sim_time, self.setup_sim_time/60.0))
 
-# Seeds for randomizers (connectivity, input stimulation and cell locations)
-# Note: locations and connections should be fully specified by the structure of the NeuroML,
-# so seeds for conn & loc shouldn't affect networks structure/behaviour
-simConfig.seeds = {'conn': 0, 'stim': 123456789, 'loc': 0} 
-
-simConfig.createNEURONObj = 1  # create HOC objects when instantiating network
-simConfig.createPyStruct = 1  # create Python structure (simulator-independent) when instantiating network
-simConfig.verbose = False  # show detailed messages 
-
-# Recording 
-simConfig.recordCells = ['all']  
-simConfig.recordTraces = {}
-simConfig.saveCellSecs=False
-simConfig.saveCellConns=False
-simConfig.gatherOnlySimData=True 
-
-# For saving to file: exIzh.dat (ref: of0)
-# Column: v: Pop: RS_pop; cell: 0; segment id: $oc.segment_id; segment name: soma; Neuron loc: soma(0.5); value: v (v)
-simConfig.recordTraces['of0_RS_pop_0_soma_v'] = {'sec':'soma','loc':0.5,'var':'v','conds':{'pop':'RS_pop','cellLabel':0}}
+        try:
+            self.save_results()
+        except Exception as e:
+            print("Exception saving results of NetPyNE simulation: %s" % (e))
+            return
 
 
+    def save_results(self):
 
-simConfig.plotCells = ['all']
+        ###############################################################################
+        #   Saving data (this ensures the data gets saved in the format/files 
+        #   as specified in the LEMS <Simulation> element)
+        ###############################################################################
 
+        if sim.rank==0: 
+        
+            print("Saving traces to file: exIzh.dat (ref: of0)")
 
-simConfig.recordStim = True  # record spikes of cell stims
-simConfig.recordStep = simConfig.dt # Step size in ms to save data (eg. V traces, LFP, etc)
+                
+            # Column: t
+            col_of0_t = [i*self.simConfig.dt for i in range(int(self.simConfig.duration/self.simConfig.dt))]
+                        
+            # Column: v: Pop: RS_pop; cell: 0; segment id: $oc.segment_id; segment name: soma; value: v
+            col_of0_v = sim.allSimData['of0_RS_pop_0_soma_v']['cell_%s'%self.gids['RS_pop'][0]]
+                
+            dat_file_of0 = open('exIzh.dat', 'w')
+            for i in range(len(col_of0_t)):
+                dat_file_of0.write( '%s\t'%(col_of0_t[i]/1000.0) +  '%s\t'%(col_of0_v[i]/1000.0) +  '\n')
+            dat_file_of0.close()
 
-
-
-# Analysis and plotting, see http://neurosimlab.org/netpyne/reference.html#analysis-related-functions
-simConfig.analysis['plotRaster'] = False  # Plot raster
-simConfig.analysis['plot2Dnet'] = False  # Plot 2D net cells and connections
-simConfig.analysis['plotSpikeHist'] = False # plot spike histogram
-simConfig.analysis['plotConn'] = False # plot network connectivity
-simConfig.analysis['plotSpikePSD'] = False # plot 3d architecture
-
-# Saving
-simConfig.filename = 'net1.txt'  # Set file output name
-simConfig.saveFileStep = simConfig.dt # step size in ms to save data to disk
-# simConfig.saveDat = True # save to dat file
-
-
-###############################################################################
-# IMPORT & RUN
-###############################################################################
-
-print("Running a NetPyNE based simulation for %sms (dt: %sms) at %s degC"%(simConfig.duration, simConfig.dt, simConfig.hParams['celsius']))
-
-setup_sim_start = time.time()
-gids = sim.importNeuroML2SimulateAnalyze(nml2_file_name,simConfig)
-
-sim_end = time.time()
-setup_sim_time = sim_end - setup_sim_start
-print("Finished NetPyNE simulation in %f seconds (%f mins)..."%(setup_sim_time, setup_sim_time/60.0))
+        
+        
+            save_end = time.time()
+            save_time = save_end - self.sim_end
+            print("Finished saving results in %f seconds"%(save_time))
 
 
-###############################################################################
-#   Saving data (this ensures the data gets saved in the format/files 
-#   as specified in the LEMS <Simulation> element)
-###############################################################################
+        
+if __name__ == '__main__':
 
+    save_json = '-json' in sys.argv
 
-if sim.rank==0: 
-    print("Saving traces to file: exIzh.dat (ref: of0)")
+    ns = NetPyNESimulation(tstop=520.0, dt=0.001, seed=123456789, save_json=save_json)
 
- 
-    # Column: t
-    col_of0_t = [i*simConfig.dt for i in range(int(simConfig.duration/simConfig.dt))]
+    ns.run()
 
-    # Column: v: Pop: RS_pop; cell: 0; segment id: $oc.segment_id; segment name: soma; value: v
-    col_of0_v = sim.allSimData['of0_RS_pop_0_soma_v']['cell_%s'%gids['RS_pop'][0]]
-
-    dat_file_of0 = open('exIzh.dat', 'w')
-    for i in range(len(col_of0_t)):
-        dat_file_of0.write( '%s\t'%(col_of0_t[i]/1000.0) +  '%s\t'%(col_of0_v[i]/1000.0) +  '\n')
-    dat_file_of0.close()
-
-
-
-    save_end = time.time()
-    save_time = save_end - sim_end
-    print("Finished saving results in %f seconds"%(save_time))
-
-
-
-if '-nogui' in sys.argv:
-    quit()
+    if '-nogui' in sys.argv:
+        quit()
