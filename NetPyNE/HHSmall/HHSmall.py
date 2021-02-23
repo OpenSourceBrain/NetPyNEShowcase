6ca04b86345477a6cbc87983a6fd854102c66486
Padraig Gleeson <p.gleeson@gmail.com>
2021-02-23 10:24:15 +0000

    Updated export

--- NetPyNE/HHSmall/HHSmall.py
+++ NetPyNE/HHSmall/HHSmall.py
@@ -1,5 +1,5 @@
 """
-params.py 
+params.py
 
 netParams is a dict containing a set of network parameters using a standardized structure
 
@@ -35,7 +35,7 @@ def generate_netParams(*argv):
     pop_size = 3 if not 'size' in argv else argv['size']
 
     # Population parameters
-    netParams.popParams['PYR'] = {'cellModel': 'HH', 'cellType': 'PYR', 'numCells': pop_size} # add dict with params for this pop 
+    netParams.popParams['PYR'] = {'cellModel': 'HH', 'cellType': 'PYR', 'numCells': pop_size} # add dict with params for this pop
 
 
     # Cell parameters
@@ -64,7 +64,7 @@ def generate_netParams(*argv):
         'delay': '0.2+normal(13.0,1.4)',     # delay min=0.2, mean=13.0, var = 1.4
         'threshold': 10,                    # threshold
         'convergence': pop_size-1}    # convergence (num presyn targeting postsyn) is uniformly distributed between 1 and 15
-        
+
     return netParams
 
 
@@ -78,9 +78,9 @@ simConfig.dt = 0.0025 # Internal integration timestep to use
 simConfig.seeds = {'conn': 1, 'stim': 1, 'loc': 1} # Seeds for randomizers (connectivity, input stimulation and cell locations)
 simConfig.createNEURONObj = 1  # create HOC objects when instantiating network
 simConfig.createPyStruct = 1  # create Python structure (simulator-independent) when instantiating network
-simConfig.verbose = False  # show detailed messages 
+simConfig.verbose = False  # show detailed messages
 
-# Recording 
+# Recording
 simConfig.recordCells = ['all']  # which cells to record from
 simConfig.recordTraces = {'Vsoma':{'sec':'soma','loc':0.5,'var':'v'}}
 simConfig.recordStim = True  # record spikes of cell stims
@@ -92,10 +92,9 @@ simConfig.saveFileStep = simConfig.dt # step size in ms to save data to disk
 simConfig.savePickle = False # Whether or not to write spikes etc. to a .mat file
 simConfig.saveDat = True # save traces
 
-# Analysis and plotting 
+simConfig.saveJson = True # save traces
+
+# Analysis and plotting
 simConfig.analysis['plotRaster'] = True  # Plot raster
 simConfig.analysis['plotTraces'] = {'include': [2]}  # Plot raster
 simConfig.analysis['plot2Dnet'] = True  # Plot 2D net cells and connections
-
-
-
--- NetPyNE/HybridSmall/HybridSmall.py
+++ NetPyNE/HybridSmall/HybridSmall.py
@@ -1,5 +1,5 @@
 """
-params.py 
+params.py
 
 netParams is a dict containing a set of network parameters using a standardized structure
 
@@ -26,8 +26,8 @@ simConfig = specs.SimConfig()   # object of class SimConfig to store the simulat
 pop_size = 3
 
 # Population parameters
-netParams.popParams['PYR_HH'] = {'cellModel': 'HH', 'cellType': 'PYR', 'numCells': pop_size} # add dict with params for this pop 
-netParams.popParams['PYR_Izhi'] = {'cellModel': 'Izhi', 'cellType': 'PYR', 'numCells': pop_size} # add dict with params for this pop 
+netParams.popParams['PYR_HH'] = {'cellModel': 'HH', 'cellType': 'PYR', 'numCells': pop_size} # add dict with params for this pop
+netParams.popParams['PYR_Izhi'] = {'cellModel': 'Izhi', 'cellType': 'PYR', 'numCells': pop_size} # add dict with params for this pop
 
 
 # Cell parameters list
@@ -37,7 +37,7 @@ cellRule['secs']['soma'] = {'geom': {}, 'topol': {}, 'mechs': {}}  # soma proper
 cellRule['secs']['soma']['geom'] = {'diam': 18.8, 'L': 18.8, 'Ra': 123.0, 'pt3d':[]}
 cellRule['secs']['soma']['geom']['pt3d'].append((0, 0, 0, 18.8))
 cellRule['secs']['soma']['geom']['pt3d'].append((0, 0, 18.8, 18.8))
-cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.12, 'gkbar': 0.036, 'gl': 0.003, 'el': -70} 
+cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.12, 'gkbar': 0.036, 'gl': 0.003, 'el': -70}
 
 netParams.cellParams['PYR_HH'] = cellRule  # add dict to list of cell properties
 
@@ -45,7 +45,7 @@ netParams.cellParams['PYR_HH'] = cellRule  # add dict to list of cell properties
 cellRule = {'conds': {'cellType': 'PYR', 'cellModel': 'Izhi'},  'secs': {}}
 cellRule['secs']['soma'] = {'geom': {}, 'pointps':{}}  # soma properties
 cellRule['secs']['soma']['geom'] = {'diam': 10, 'L': 10, 'cm': 31.831}
-cellRule['secs']['soma']['pointps']['Izhi'] = {'mod':'Izhi2007b', 
+cellRule['secs']['soma']['pointps']['Izhi'] = {'mod':'Izhi2007b',
     'C':1, 'k':0.7, 'vr':-60, 'vt':-40, 'vpeak':35, 'a':0.03, 'b':-2, 'c':-50, 'd':100, 'celltype':1}
 netParams.cellParams['PYR_Izhi'] = cellRule  # add dict to list of cell properties
 
@@ -53,14 +53,14 @@ netParams.cellParams['PYR_Izhi'] = cellRule  # add dict to list of cell properti
 # Synaptic mechanism parameters
 netParams.synMechParams['syn1'] = {'mod': 'ExpSyn', 'tau': 30, 'e': 0}
 netParams.synMechParams['syn2'] = {'mod': 'ExpSyn', 'tau': 4, 'e': 0}
- 
+
 
 # Stimulation parameters
 netParams.stimSourceParams['bkg1'] = {'type': 'NetStim', 'rate': 20, 'noise': 0}
 netParams.stimSourceParams['bkg2'] = {'type': 'NetStim', 'rate': 20, 'noise': 1}
-netParams.stimTargetParams['bg->PYR_Izhi'] = {'source': 'bkg1', 'conds': {'cellType': 'PYR', 'cellModel': 'Izhi'}, 
-                                            'connFunc': 'fullConn','weight': 0.01, 'delay': 0, 'synMech': 'syn2'}  
-netParams.stimTargetParams['bg->PYR_HH'] = {'source': 'bkg2', 'conds': {'cellType': 'PYR', 'cellModel': 'HH'}, 
+netParams.stimTargetParams['bg->PYR_Izhi'] = {'source': 'bkg1', 'conds': {'cellType': 'PYR', 'cellModel': 'Izhi'},
+                                            'connFunc': 'fullConn','weight': 0.01, 'delay': 0, 'synMech': 'syn2'}
+netParams.stimTargetParams['bg->PYR_HH'] = {'source': 'bkg2', 'conds': {'cellType': 'PYR', 'cellModel': 'HH'},
                                             'connFunc': 'fullConn','weight': 0.005, 'synMech': 'syn1', 'sec': 'soma', 'loc': 1.0, 'delay': 0}
 
 
@@ -71,7 +71,7 @@ netParams.connParams['PYR->PYR'] = {
     'delay': '0.2+normal(13.0,1.4)',     # delay min=0.2, mean=13.0, var = 1.4
     'threshold': 10,                    # threshold
     'convergence': 'uniform(0,5)',       # convergence (num presyn targeting postsyn) is uniformly distributed between 1 and 10
-    'synMech': 'syn1'}    
+    'synMech': 'syn1'}
 
 
 
@@ -87,32 +87,31 @@ simConfig.seeds = {'conn': 1, 'stim': 1, 'loc': 1} # Seeds for randomizers (conn
 simConfig.createNEURONObj = True  # create HOC objects when instantiating network
 simConfig.createPyStruct = True  # create Python structure (simulator-independent) when instantiating network
 simConfig.timing = True  # show timing  and save to file
-simConfig.verbose = False # show detailed messages 
+simConfig.verbose = False # show detailed messages
 
 
-# Recording 
-simConfig.recordCells = []  # list of cells to record from 
-simConfig.recordTraces = {'V':{'sec':'soma','loc':0.5,'var':'v'}, 
-    'u':{'sec':'soma', 'pointp':'Izhi', 'var':'u'}, 
-    'I':{'sec':'soma', 'pointp':'Izhi', 'var':'i'}, 
+# Recording
+simConfig.recordCells = []  # list of cells to record from
+simConfig.recordTraces = {'V':{'sec':'soma','loc':0.5,'var':'v'},
+    'u':{'sec':'soma', 'pointp':'Izhi', 'var':'u'},
+    'I':{'sec':'soma', 'pointp':'Izhi', 'var':'i'},
     'AMPA_g': {'sec':'soma', 'loc':0.5, 'synMech':'AMPA', 'var':'g'},
     'AMPA_i': {'sec':'soma', 'loc':0.5, 'synMech':'AMPA', 'var':'i'}}
 simConfig.recordStim = True  # record spikes of cell stims
 simConfig.recordStep = simConfig.dt # Step size in ms to save data (eg. V traces, LFP, etc)
 
 # Saving
-simConfig.filename = 'mpiHybridTut'  # Set file output name
+simConfig.filename = 'HybridTut'  # Set file output name
 simConfig.saveFileStep = simConfig.dt # step size in ms to save data to disk
 simConfig.savePickle = False # Whether or not to write spikes etc. to a .mat file
-simConfig.saveJson = False # Whether or not to write spikes etc. to a .mat file
+simConfig.saveJson = True # Whether or not to write spikes etc. to a .mat file
 simConfig.saveMat = False # Whether or not to write spikes etc. to a .mat file
 simConfig.saveTxt = False # save spikes and conn to txt file
 simConfig.saveDpk = False # save to a .dpk pickled file
 simConfig.saveDat = True
 
 
-# Analysis and plotting 
+# Analysis and plotting
 simConfig.analysis['plotRaster'] = {'orderInverse': False} #True # Whether or not to plot a raster
 simConfig.analysis['plotTraces'] = {'include': [1,51]} # plot recorded traces for this list of cells
-simConfig.analysis['plotRatePSD'] = {'include': ['allCells', 'PYR_HH', 'PYR_Izhi'], 'Fs': 200, 'smooth': 10} # plot recorded traces for this list of cells
-
+#simConfig.analysis['plotRatePSD'] = {'include': ['allCells', 'PYR_HH', 'PYR_Izhi'], 'smooth': 10} # plot recorded traces for this list of cells
