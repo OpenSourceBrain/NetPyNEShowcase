b2f78706ea9799a13f7179bd62cc12d5c410335a
Padraig Gleeson <p.gleeson@gmail.com>
2021-04-21 12:53:24 +0100

    Better commented & generated with v0.9.9.1

--- NetPyNE/UI/reload3.py
+++ NetPyNE/UI/reload3.py
@@ -3,16 +3,16 @@
 import logging
 from LEMS_HHSimple_netpyne import NetPyNESimulation
 
-logging.info('Loading from python generated from jnml...')
+logging.info(' >> Loading from python generated from jnml...')
 ns = NetPyNESimulation(tstop=450.0, dt=0.05, seed=123456789, save_json=False)
 
 simConfig, netParams = ns.run()
-logging.info('Finished run!...')
+logging.info(' >> Finished run!...')
 
 
-print(' - simConfig (%s) with keys: \n      %s'%(type(simConfig),simConfig.todict().keys()))
-print(' - netParams (%s) with keys: \n      %s'%(type(netParams),netParams.todict().keys()))
-print(' - netParams.cellParams: \n      %s'%(netParams.cellParams))
+print(' >> simConfig (%s) with keys: \n      %s'%(type(simConfig),simConfig.todict().keys()))
+print(' >> netParams (%s) with keys: \n      %s'%(type(netParams),netParams.todict().keys()))
+print(' >> netParams.cellParams: \n      %s'%(netParams.cellParams))
 
 
-logging.info('> Done...')
+logging.info(' >> Done...')
--- NetPyNE/UI/test_all.sh
+++ NetPyNE/UI/test_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-# This python script was exported from NetPyNE UI & runsd fine
+# This python script was exported from NetPyNE UI & runs fine
 python NetPyNE_init.py -run
 # JSON generated following that simulation is reloaded and run
 python reload.py
