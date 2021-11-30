7431a4fb0c6edcc486b581d4b52bf18dc849bb4e
Padraig Gleeson <p.gleeson@gmail.com>
2021-11-30 13:12:09 +0000

    Use pynml in test

--- NetPyNE/UI/test_all.sh
+++ NetPyNE/UI/test_all.sh
@@ -9,9 +9,9 @@ python reload.py
 
 
 # A LEMS simulation for a simple NML network is run in various ways
-jnml LEMS_HHSimple.xml -nogui                 # run with jNeuroML
-jnml LEMS_HHSimple.xml -neuron -run -nogui    # run in NEURON
-jnml LEMS_HHSimple.xml -netpyne -run -nogui   # run in NetPyNE
+pynml LEMS_HHSimple.xml -nogui                 # run with jNeuroML
+pynml LEMS_HHSimple.xml -neuron -run -nogui    # run in NEURON
+pynml LEMS_HHSimple.xml -netpyne -run -nogui   # run in NetPyNE
 
 # This python script was generated above & is rerun with -json option & so the model is saved to json
 python LEMS_HHSimple_netpyne.py -json
