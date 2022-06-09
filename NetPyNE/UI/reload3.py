a89affec49e633ec1f2b4a033eb9cdd92c3722c7
Padraig Gleeson <p.gleeson@gmail.com>
2022-06-09 16:28:05 +0100

    More tests

--- NetPyNE/UI/reload3.py
+++ NetPyNE/UI/reload3.py
@@ -5,13 +5,23 @@ from LEMS_HHSimple_netpyne import NetPyNESimulation
 logging.info(' >> Loading from python generated from jnml...')
 ns = NetPyNESimulation(tstop=450.0, dt=0.05, seed=123456789, save_json=False)
 
-ns.run()
-logging.info(' >> Finished run!...')
+#ns.run()
 
+simConfig = ns.simConfig
+fileName = ns.nml2_file_name
 
-#print(' >> simConfig (%s) with keys: \n      %s'%(type(simConfig),simConfig.todict().keys()))
-#print(' >> netParams (%s) with keys: \n      %s'%(type(netParams),netParams.todict().keys()))
-#print(' >> netParams.cellParams: \n      %s'%(netParams.cellParams))
+from netpyne.conversion.neuromlFormat import importNeuroML2
 
+gids, netParams = importNeuroML2(fileName, simConfig, simulate=False, analyze=False, return_net_params_also=True)
+
+logging.info(' >> Finished import!...')
+
+print(' - simConfig (%s) with keys: \n      %s'%(type(simConfig),simConfig.todict().keys()))
+print(' - netParams (%s) with keys: \n      %s'%(type(netParams),netParams.todict().keys()))
+
+from netpyne.sim.save import saveData
+simConfig.saveJson = True
+
+saveData(filename='test.json', include=['simConfig','netParams','net'])
 
 logging.info(' >> Done...')
