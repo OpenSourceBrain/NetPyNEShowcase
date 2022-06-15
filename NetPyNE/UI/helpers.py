3b6bc8ff8f340ea8d9bf74cd4001efa18b0d2b60
Padraig Gleeson <p.gleeson@gmail.com>
2022-06-15 10:20:56 +0100

    Restructure helpers

--- NetPyNE/UI/helpers.py
+++ NetPyNE/UI/helpers.py
@@ -1,48 +1,98 @@
-
-
 from pyneuroml import pynml
+import os
+import shutil
+import subprocess
+import neuron
 
-def convertAndImportLEMSSimulation(lemsFileName, simulate=True, analyze=True):
 
-    print("Importing LEMSSimulation with NeuroML 2 network from: %s"%lemsFileName)
+def convertAndImportLEMSSimulation(
+    lemsFileName, simulate=False, analyze=False, verbose=True
+):
 
-    results1 = pynml.run_lems_with_jneuroml_netpyne(
-        lemsFileName, only_generate_scripts=True
-    )
-    netpyne_file = lemsFileName.replace('.xml', '_netpyne')
+    fullLemsFileName = os.path.abspath(lemsFileName)
+    if verbose:
+        print(
+            "Importing LEMSSimulation with NeuroML 2 network from: %s"
+            % fullLemsFileName
+        )
 
-    import_str = 'from %s import NetPyNESimulation'%netpyne_file
-    '''
-    import importlib
-    importlib.import_module('%s'%netpyne_file)
-    from LEMS_HHSimple_netpyne import NetPyNESimulation'''
+    pynml.run_lems_with_jneuroml_netpyne(lemsFileName, only_generate_scripts=True)
+    netpyne_file = lemsFileName.replace(".xml", "_netpyne")
+
+    compileModMechFiles(compileMod=True, modFolder=os.path.dirname(fullLemsFileName))
+
+    #print(os.getcwd())
+
+    import_str = "from %s import NetPyNESimulation" % netpyne_file
 
     exec(import_str, globals())
 
-    print('Loading from python generated from jnml (using: %s)...'%netpyne_file)
+    if verbose:
+        print("Loading from python generated from jnml (using: %s)..." % import_str)
 
-    ns = eval('NetPyNESimulation(tstop=450.0, dt=0.05, seed=1234, save_json=False)')
-
-    print(ns)
+    ns = eval("NetPyNESimulation(tstop=1000, dt=0.025)")
 
     simConfig = ns.simConfig
     fileName = ns.nml2_file_name
 
     from netpyne.conversion.neuromlFormat import importNeuroML2
 
-    gids, netParams = importNeuroML2(fileName, simConfig, simulate=False, analyze=False, return_net_params_also=True)
+    gids, netParams = importNeuroML2(
+        fileName,
+        simConfig,
+        simulate=simulate,
+        analyze=analyze,
+        return_net_params_also=True,
+    )
 
-    print(' >> Finished NeuroML/LEMS import!...')
+    if verbose:
+        print("Finished NeuroML/LEMS import!...")
 
-    print(' - simConfig (%s) with keys: \n      %s'%(type(simConfig),simConfig.todict().keys()))
-    print(' - netParams (%s) with keys: \n      %s'%(type(netParams),netParams.todict().keys()))
+        print(
+            " - simConfig (%s) with keys: \n      %s"
+            % (type(simConfig), simConfig.todict().keys())
+        )
+        print(
+            " - netParams (%s) with keys: \n      %s"
+            % (type(netParams), netParams.todict().keys())
+        )
 
     from netpyne.sim.save import saveData
+
     simConfig.saveJson = True
 
-    saveData(filename='test.json', include=['simConfig','netParams','net'])
+    saveData(filename="test.json", include=["simConfig", "netParams", "net"])
 
-    print(' >> Done...')
+
+def compileModMechFiles(compileMod, modFolder):
+    # Create Symbolic link
+    if compileMod:
+        modPath = os.path.join(str(modFolder), "x86_64")
+
+        if os.path.exists(modPath):
+            print("Deleting existing %s"%modPath)
+            shutil.rmtree(modPath)
+
+        os.chdir(modFolder)
+        subprocess.call(["nrnivmodl"])
+
+        try:
+            neuron.load_mechanisms(str(modFolder))
+            print("Loaded mod file mechanisms from %s" % modFolder)
+        except:
+            print(
+                "************************************************************\n"
+                +"Error loading the newly generated mod file mechanisms from folder: %s"
+                % modFolder
+            )
+            print(
+                "\nNote: if this is the current folder, this may be due to a preexisting "
+                + "mod file of the same name being already compiled in that folder and "
+                + "NEURON attempting to load it agian. \nRemove any previous x86_64 etc. "
+                + "directories before calling this method.\n"+
+                "************************************************************"
+            )
+            raise
 
 
 if __name__ == "__main__":
